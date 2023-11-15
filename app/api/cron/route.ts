5fb424fedc75415f099f059e15495e919e9c0fb2
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-11-15 23:39:31 +0530

    updates on max duration for serverless functions

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 } from "@/lib/helper";
 import { generateEmailBody, sendEmail } from "@/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
